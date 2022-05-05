a5e7b63834389334269f05c457c3dc88c6ad11c0
Chance <cestep4328@gmail.com>
2022-05-05 12:16:04 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -16,29 +16,30 @@ public class Main {
         System.out.println("Hello " + player1 + " I want to play a game.\n" +
                 "I am thinking of a number between 1 - 20.\n" +
                 "you have 6 tries to get it right...");
-        
-        int attemtpt = 0;
-        while(true) {
+
+        int attempt = 0;
+        while (true) {
             System.out.print("Take a guess: ");
             Scanner firstGuess = new Scanner(System.in); // get player1 guess
             String guessStr = name.nextLine(); // read guess
             int guess1 = Integer.parseInt(guessStr);
-            
-            if ( guess1 == randomNumber ) {
+
+            if (guess1 == randomNumber) {
                 System.out.println("lucky guess... You win :)"); // win end game
                 break;
-                
-            } else if ( guess1 < randomNumber ) {
+
+            } else if (guess1 < randomNumber) {
                 System.out.println("Too low, try again. I insist.");
-                attemtpt++;
-                
-            } else if ( attemtpt >= 6 ){
-                System.out.print("Game over. Attempt limit reached. My number was " + randomNumber );
+                attempt++;
+
+            } else if (attempt >= 6) {
+                System.out.print("Game over. Attempt limit reached. My number was " + randomNumber);
                 break;
-               
+
             } else {
                 System.out.println("Too high, try again. I insist.");
-                attemtpt++;
+                attempt++;
+            }
         }
     }
 }
